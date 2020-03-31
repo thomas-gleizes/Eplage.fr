94f3f59ff0d43c298fadb8e71a71df0088d66c56
thomas <thomas.gleizes@etu.umontpellier.fr>
2020-03-31 14:17:33 +0200

    genereta card

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,40 @@
 
 
 document.getElementById("search-input").addEventListener('keyup', function(){
+    let key = event.keyCode;
+    if (key >= 65 || key <= 90) document.getElementById("list").innerHTML = "";
     if (this.value.length > 1){
         selectPlage(this.value);
     }
 });
 
 function createCard (tab){
+    let list = document.getElementById("list");
+    if (tab.length === 0){
+
+    } else {
+        for (let i = 0; i < tab.length; i++){
+            let card = document.createElement("div");
+            let src = tab[i].src.split('¤')[0];
+            card.innerHTML = "<div class='card'>\n" +
+                "                <div class='card-image'>\n" +
+                "                    <img src= " + src + "\n" +
+                "                    <a class='btn-floating halfway-fab waves-effect waves-light red'><i class='material-icons'>chevron_right</i></a>\n" +
+                "                </div>\n" +
+                "                <div class='card-content'>\n" +
+                "                    <span class='card-title'> " + tab[i].NAME + "</span>\n" +
+                "                    <p>La plage 1 est ouvert de 9h à 18h</p>\n" +
+                "                </div>\n" +
+                "                <div class='card-action'>\n" +
+                "                    <i class='material-icons small'>map</i> <div>" + tab[i].CITY + " (" + tab[i].ZIPCODE.substring(0,2) + ") </div>\n" +
+                "                </div>\n" +
+                "            </div>"
+            card.id = "Plage-" + tab[i].ID;
+            card.className = "col S12 m6 l3";
+            list.appendChild(card);
+        }
+    }
+
 
 }
 
