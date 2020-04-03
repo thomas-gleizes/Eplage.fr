34b0169c0093c781251595b7f0596ac4120b1b9b
thomas <thomas.gleizes@etu.umontpellier.fr>
2020-04-03 14:34:30 +0200

    refactor front

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,7 @@
+let sea = "#2ecbfe";
+let sand = "#ffd59b";
+let geo = "false";
 let charge = true;
-let filter = [];
-let filterActive = false;
-
-
-for (let i = 1; i < 21; i++) {
-    let li = document.getElementById("filter-" + i);
-    li.addEventListener("mouseover", function () {
-        this.style.border = "solid 2px #ffd774";
-    });
-    li.addEventListener("mouseout", function () {
-        if (filter.indexOf(this.id.split('-')[1]) === -1) this.style.border = "solid 1px silver";
-        else this.style.border = "solid 1.5px #ffd59b";
-    });
-    li.addEventListener("click", function () {
-        let id = this.id.split('-')[1];
-        if (filter.indexOf(id) === -1) {
-            filter.push(id);
-            this.style.border = "solid 1.5px #ffd59b";
-        } else {
-            filter.splice(filter.indexOf(id), 1);
-            this.style.border = "solid 1px silver";
-        }
-
-    });
-}
 
 
 document.getElementById("search-input").addEventListener('keyup', function () {
@@ -60,19 +38,28 @@ function createCard(tab) {
         for (let i = 0; i < tab.length; i++) {
             let card = document.createElement("div");
             let src = tab[i].src.split('¤')[0];
-            card.innerHTML = "<div class='card'>\n" +
+            let HTML = "<div class='card'>\n" +
                 "                <div class='card-image'>\n" +
                 "                    <img src= \"" + src + "\"  />\n" +
-                "                    <a class='btn-floating halfway-fab waves-effect waves-light bluet'><i class='material-icons'>chevron_right</i></a>\n" +
+                "                    <a class='btn-floating halfway-fab waves-effect waves-light sea'><i class='material-icons'>chevron_right</i></a>\n" +
                 "                </div>\n" +
                 "                <div class='card-content'>\n" +
                 "                    <span class='card-title'> " + tab[i].NAME + "</span>\n" +
                 "                    <p>La plage 1 est ouvert de 9h à 18h</p>\n" +
                 "                </div>\n" +
                 "                <div class='card-action'>\n" +
-                "                    <i class='material-icons small'>map</i> <div>" + tab[i].CITY + " (" + tab[i].ZIPCODE.substring(0, 2) + ") </div>\n" +
+                "                    <i class='material-icons small'>map</i> <div>" + tab[i].CITY + " (" + tab[i].ZIPCODE.substring(0, 2) + ") </div>\n";
+            if (tab[i].FLEACHID !== 0) {
+                //TODO nombre de transate disponnible
+                //HTML += "<div class='count'><i class='count-icons material-icons small left'>beach_access</i> ID </div>\n ";
+            } else {
+                //TODO afficher message
+                //HTML += "";
+            }
+            HTML += "          <div class='count'><i class='count-icons material-icons small left'>beach_access</i> ID </div>\n " +
                 "                </div>\n" +
                 "            </div>";
+            card.innerHTML = HTML;
             card.id = "Plage-" + tab[i].ID;
             card.className = "col S12 m6 l3";
             list.appendChild(card);
@@ -82,36 +69,43 @@ function createCard(tab) {
 }
 
 
-document.getElementById("add-filter").addEventListener('click', function () {
-    if (!filterActive) {
-        filterActive = true;
-        this.innerHTML = "<i class=\"material-icons left\">list</i>Enlever filtre";
-        document.getElementById("filter").style.display = "inline";
-    } else {
-        filterActive = false;
-        this.innerHTML = "<i class=\"material-icons left\">list</i>Ajouter filtre";
-        document.getElementById("filter").style.display = "none";
+document.getElementById("search-btn").addEventListener("mouseover", function () {
+    this.style.backgroundColor = sea
+});
+
+document.getElementById("search-btn").addEventListener("mouseout", function () {
+    this.style.backgroundColor = sand
+});
+
+
+document.getElementById("get-btn").addEventListener("mouseout", function () {
+    if (!geo) {
+
     }
 });
 
-document.getElementById("filter").addEventListener("mouseover", function () {
-    document.getElementById("list-filter").style.display = "block"
-});
-
-document.getElementById("filter").addEventListener("mouseout", function () {
-    document.getElementById("list-filter").style.display = "none"
-});
-
-
-document.getElementById('btn-proxi').addEventListener("click", function () {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            selectProximity(position.coords.longitude, position.coords.latitude);
-        });
+document.getElementById("geo-btn").addEventListener("click", function () {
+    if (!geo) {
+        geo = true;
+        this.style.backgroundColor = "#FF5F6D"
     } else {
-        alert("echec de localisation")
+        geo = false;
+        this.style.backgroundColor = sand;
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
                                                         
